b682a80778c95af42bc75f323c2b5690a1298181
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-17 22:39:47 -0500

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,20 @@
+import { Navbar } from "react-bootstrap";
+import logo from "../assets/logo.ico";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Navbar expand="lg" width="50px" height="50px">
+        <div>
+          {" "}
+          <Navbar.Brand>
+            <img src={logo} alt="TheJoyOfHair" width="50px" height="50px" />
+            Connor's Shop
+          </Navbar.Brand>
+        </div>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
