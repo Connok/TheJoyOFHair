6d80d7510c2a891988c3db58c4b2a900a74974f5
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-19 00:56:10 -0500

    HIDE IMAGE

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 const Service = ({ service }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Card.Img src={service.image} variant="top" />
+      {/* <Card.Img src={service.image} variant="top" />*/}
 
       <Card.Body>
         <Card.Title as="div">
