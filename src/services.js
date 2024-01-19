1a8a9fa197cca65ed42313dbdb96283f2d5b519a
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-18 22:05:43 -0500

    serveces

--- src/services.js
+++ src/services.js
@@ -53,6 +53,60 @@ const services = [
         image: '/images/sample.jpg',
         price: 100.00,
     },
+    {
+        _id: '10',
+        serviceName: "Highlights Full Head",
+        image: '/images/sample.jpg',
+        price: 130.00,
+    },
+    {
+        _id: '11',
+        serviceName: "Back TO Back Foils",
+        image: '/images/sample.jpg',
+        price: 150.00,
+    },
+    {
+        _id: '12',
+        serviceName: "Cap Highlights",
+        image: '/images/sample.jpg',
+        price: 80.00,
+    },
+    {
+        _id: '13',
+        serviceName: "Perm Short Hair",
+        image: '/images/sample.jpg',
+        price: 75.00,
+    },
+    {
+        _id: '14',
+        serviceName: "Perm Long Head",
+        image: '/images/sample.jpg',
+        price: 85.00,
+    },
+    {
+        _id: '15',
+        serviceName: "Mens Flash",
+        image: '/images/sample.jpg',
+        price: 40.00,
+    },
+    {
+        _id: '16',
+        serviceName: "Set or Style",
+        image: '/images/sample.jpg',
+        price: 20.00,
+    },
+    {
+        _id: '17',
+        serviceName: "Foils",
+        image: '/images/sample.jpg',
+        price: 5.00,
+    },
+    {
+        _id: '18',
+        serviceName: "Bangs",
+        image: '/images/sample.jpg',
+        price: 5.00,
+    },
 
 ]
 export default services;
