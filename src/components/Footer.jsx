67e6a3abf7577c880c6abd279818814008055a1e
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-20 22:02:06 -0500

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,7 +9,11 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">
-            <p>The Joy Of Hair &copy; {currentYear}</p>
+            <p>The Joy Of Hair &copy; {currentYear} </p>
+            <p>
+              <FaInstagram href="https://www.instagram.com/the.joy.of.hair/" />{" "}
+              <FaFacebook href="https://www.facebook.com/thejoyofhairjoy" />
+            </p>
           </Col>
         </Row>
       </Container>
